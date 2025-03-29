488466469ddfd0b5c090b09ae7d277418d59f610
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-03-29 00:21:53 -0500

    mejora componente select para manejar id y name

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 
 interface SelectProps {
-  name: string; // 🔹 Agregar el nombre del select
-  value: string;
+  name: string;
+  value: string | number;
   onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
-  options: string[];
+  options: { id: number; name: string }[];
 }
 
 export const Select: React.FC<SelectProps> = ({ name, value, onChange, options }) => {
   return (
     <select
-      name={name} // 🔹 Añadir name aquí
+      name={name}
       value={value}
       onChange={onChange}
       className="border border-gray-300 p-2 w-full rounded bg-white focus:border-2 focus:border-[#78211E]"
     >
       {options.map((op) => (
-        <option key={op} value={op}>
-          {op}
+        <option key={op?.id ?? op} value={op?.id ?? op}>
+          {op?.name ?? op}
         </option>
       ))}
     </select>
