9d4072998c671caf22f199a5f0b1d818920d00eb
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-04-14 09:40:22 -0500

    modifica Button.jsx

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-export function Button({ children, ...props }: Props) {
+export const Button = ({ children, ...props }) => {
   return (
     <button
       className="bg-[#78211E] text-white px-3 py-2 rounded hover:bg-[rgb(90,24,21)] cursor-pointer select-none"
