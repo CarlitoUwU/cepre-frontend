4c8912ae9ada8cf9ce6dbe3626f385bbadff8bb5
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-04-14 09:52:45 -0500

    modifica import

--- src/hooks/useProfesores.js
+++ src/hooks/useProfesores.js
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { TeachersServices } from "@/services/TeachersServices";
+import { TeachersServices } from "@/services/TeachersServices.js";
 
 export const useProfesores = ({ page = 1, limit = 20 } = {}) => {
   const queryClient = useQueryClient();
