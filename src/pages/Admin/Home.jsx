dc0b9d5046d964088e1893975dee95734b4bd6d6
Jorge <jcondorios@unsa.edu.pe>
2025-04-18 22:08:03 -0500

    vista home admin cel

--- src/pages/Admin/Home.jsx
+++ src/pages/Admin/Home.jsx
@@ -5,7 +5,7 @@ const Button = ({ path, title, img, handleClick }) => {
   return (
     <button
       onClick={() => handleClick(path)}
-      className="flex flex-col items-center justify-center w-60 h-60 bg-gray-200 rounded-xl shadow-lg hover:bg-gray-300 transition-all cursor-pointer mx-auto" // Added mx-auto here
+      className="flex flex-col items-center justify-center w-56 h-60 bg-gray-200 rounded-xl shadow-lg hover:bg-gray-300 transition-all cursor-pointer"
     >
       <img src={img} alt={title} className="w-16 h-16 mb-3" />
       <p className="text-lg font-semibold">{title}</p>
@@ -28,15 +28,16 @@ export const Home = () => {
   return (
     <div className="md:m-5">
       <div className="w-full min-h-screen md:min-h-[83vh] flex flex-col items-center justify-center bg-[#F4F4F4] md:rounded-lg">  
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-16 gap-y-8 p-4 md:p-8 w-full place-items-center"> {/* Changed to place-items-center */}
+        <div className="flex flex-col sm:flex-row flex-wrap justify-center gap-x-16 gap-y-8 p-4 w-full">
           {options.map((option, index) => (
-            <Button
-              key={index}
-              path={option.path}
-              title={option.title}
-              img={option.img}
-              handleClick={handleClick}
-            />
+            <div key={index} className="sm:mx-2 flex justify-center"> {/* Added flex justify-center for mobile */}
+              <Button
+                path={option.path}
+                title={option.title}
+                img={option.img}
+                handleClick={handleClick}
+              />
+            </div>
           ))}
         </div>
       </div>
