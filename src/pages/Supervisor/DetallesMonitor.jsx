54aa98ae03c09834ab2fb4d97824b7794155f557
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-04-28 23:57:05 -0500

    agrega profesor en horario por dia, el numero del profe y mejora la tabla de index

--- src/pages/Supervisor/DetallesMonitor.jsx
+++ src/pages/Supervisor/DetallesMonitor.jsx
@@ -1,85 +1,50 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import { TablaHorarioMonitor } from "@/components/Horarios/indexMonitor";
 import { Tabla } from "@/components/ui/Tabla";
-import { MonitorsServices } from "@/services/MonitorsServices";
 import { ButtonNegative } from "@/components/ui/ButtonNegative";
-import { DIAS_DIC } from "@/constants/dias";
-import { formatTimeToHHMM } from "@/utils/formatTime";
+import { useHorarioYDocentes } from "@/hooks/useHorarioYDocentes";
+import { SkeletonTabla } from "@/components/skeletons/SkeletonTabla";
 
-export const DetallesMonitor = ({ aula = {}, volver = () => {} }) => {
+export const DetallesMonitor = ({ aula = {}, volver = () => { } }) => {
   const [horario, setHorario] = useState([]);
   const [docentes, setDocentes] = useState([]);
+  const { data, isLoading } = useHorarioYDocentes(aula?.id);
 
   useEffect(() => {
-    if (!aula || !aula.id) return;
+    const docentesTabla = data?.docentes?.map((doc, i) => [
+      i + 1,
+      doc.curso,
+      doc.docente,
+      doc.correo,
+      doc.telefono,
+    ]);
 
-    const fetchHorarioData = async (salonId) => {
-      try {
-        const horarioData = await MonitorsServices.cargarHorario(salonId);
-        if (Array.isArray(horarioData)) {
-          setHorario(
-            horarioData.map((hora) => ({
-              dia: DIAS_DIC[hora.weekday] || "Día desconocido",
-              hora_ini: formatTimeToHHMM(hora.startTime),
-              hora_fin: formatTimeToHHMM(hora.endTime),
-              curso: hora.courseName || "Curso desconocido",
-            }))
-          );
-        } else {
-          console.error("Respuesta inválida de la API (horario):", horarioData);
-        }
-      } catch (error) {
-        console.error("Error al obtener horarios:", error);
-      }
-    };
+    setDocentes(docentesTabla);
+    setHorario(data?.horario);
+  }, [aula, data]);
 
-    const fetchDocentesData = async (salonId) => {
-      try {
-        const docentesData = await MonitorsServices.cargarDocentes(salonId);
-        if (Array.isArray(docentesData)) {
-          setDocentes(
-            docentesData.map((docente, index) => [
-              index + 1,
-              docente.courseName || "Curso desconocido",
-              `${docente.firstName || "Nombre"} ${docente.lastName || "Desconocido"}`,
-              docente.email || "Sin correo",
-              docente.phoneNumber || "Sin número",
-            ])
-          );
-        } else {
-          console.error("Respuesta inválida de la API (docentes):", docentesData);
-        }
-      } catch (error) {
-        console.error("Error al obtener docentes:", error);
-      }
-    };
-
-    fetchHorarioData(aula.id);
-    fetchDocentesData(aula.id);
-  }, [aula]);
-
-  const horariosRender = useMemo(() => horario, [horario]);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-5 text-sm">
 
       {/* Directorio del Aula (Izquierda - menos ancho) */}
       <div className="md:col-span-2 flex flex-col items-center mb-4 md:mb-0">
-      <h2 className="text-lg font-semibold text-center mb-4">
+        <h2 className="text-lg font-semibold text-center mb-4">
           {`Directorio`}
         </h2>
         <div className="w-full max-w-[100%]">
-          <Tabla encabezado={["N°", "Curso", "Nombre", "Correo", "Número"]} datos={docentes} />
+          {isLoading ? <SkeletonTabla />
+            : <Tabla encabezado={["N°", "Curso", "Nombre", "Correo", "Número"]} datos={docentes} />}
         </div>
       </div>
 
       {/* Horario del Aula (Derecha - más ancho) */}
       <div className="md:col-span-3 flex flex-col items-center">
-      <h2 className="text-lg font-semibold text-center mb-4">
+        <h2 className="text-lg font-semibold text-center mb-4">
           {`Horario`}
         </h2>
         <div className="w-full max-w-[95%]">
-          <TablaHorarioMonitor horas={horariosRender} />
+          <TablaHorarioMonitor horas={horario} />
         </div>
 
         {/* Botón Volver */}
@@ -87,7 +52,6 @@ export const DetallesMonitor = ({ aula = {}, volver = () => {} }) => {
           <ButtonNegative onClick={volver}>Volver</ButtonNegative>
         </div>
       </div>
-
     </div>
   );
 };
