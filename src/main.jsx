b36c7740dc1c5bac17b514e8418f0ca920f4a72c
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-05-14 16:26:59 -0500

    agrega vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import "./index.css";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ErrorBoundary } from "./ErrorBoundary";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <AuthProvider >        
         <RouterProvider router={router} />
         <ToastContainer />
+        <Analytics />
       </AuthProvider>
     </QueryClientProvider>
   </ErrorBoundary>
